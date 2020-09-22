83cd7bca50c4cf435a9d40d68b47112cab8557e4
Jone Wang <i@jone.wang>
2020-09-22 23:56:21 +0800

    Battle list performance optimization.

--- imink/Shared/Views/Battle/Views/RecordRow.swift
+++ imink/Shared/Views/Battle/Views/RecordRow.swift
@@ -15,94 +15,89 @@ struct RecordRow: View {
     
     var body: some View {
         let contentView = ZStack {
-            if let battle: SP2Battle = record.battle {
-                let barValue = Double(battle.myPoint) &/ Double((battle.myPoint + battle.otherPoint))
-                
-                VStack(spacing: 2) {
-                    ZStack {
-                        HStack {
-                            Text("\(battle.battleNumber)")
-                                .sp2Font(color: Color.secondary)
-                            Spacer()
-                        }
-                        .opacity(0.5)
-                        
-                        Text("\(battle.otherTeamResult.name)")
-                            .sp1Font(
-                                size: 20,
-                                color: battle.myTeamResult.key == .victory ?
-                                    AppColor.spPink :
-                                    AppColor.spLightGreen
-                            )
+            let barValue = Double(record.myPoint) &/ Double((record.myPoint + record.otherPoint))
+            
+            VStack(spacing: 2) {
+                ZStack {
+                    HStack {
+                        Text("\(record.battleNumber)")
+                            .sp2Font(color: Color.secondary)
+                        Spacer()
                     }
-                    .padding([.leading, .top], 10)
+                    .opacity(0.5)
                     
-                    HStack(alignment: .top) {
-                        VStack {
-                            Text("\(battle.gameMode.name)")
-                                .sp1Font(color: Color.secondary)
-                            
-                            Spacer()
-                            
-                            Text("\(battle.playerResult.killCount + battle.playerResult.assistCount) k  \(battle.playerResult.deathCount) d")
-                                .sp2Font(
-                                    size: 20,
-                                    color: isSelected ?
-                                        AppColor.recordRowTitleSelectedColor :
-                                        AppColor.recordRowTitleNormalColor
-                                )
-                        }
+                    Text("\(record.victory ? "VICTORY" : "DEFEAT")")
+                        .sp1Font(
+                            size: 20,
+                            color: record.victory ?
+                                AppColor.spPink :
+                                AppColor.spLightGreen
+                        )
+                }
+                .padding([.leading, .top], 10)
+                
+                HStack(alignment: .top) {
+                    VStack {
+                        Text("\(record.gameMode)")
+                            .sp1Font(color: Color.secondary)
                         
                         Spacer()
                         
-                        VStack(alignment: .trailing) {
-                            Text("\(battle.rule.name)")
-                                .sp1Font(color: Color.secondary)
-                            
-                            Spacer()
-                            
-                            Text("\(battle.stage.name)")
-                                .sp2Font(
-                                    color: isSelected ?
-                                        AppColor.recordRowTitleSelectedColor :
-                                        AppColor.recordRowTitleNormalColor
-                                )
-                                .padding(.bottom, 5)
-                        }
+                        Text("\(record.killTotalCount) k  \(record.deathCount) d")
+                            .sp2Font(
+                                size: 20,
+                                color: isSelected ?
+                                    AppColor.recordRowTitleSelectedColor :
+                                    AppColor.recordRowTitleNormalColor
+                            )
                     }
-                    .padding([.leading, .trailing], 10)
                     
                     Spacer()
                     
-                    GeometryReader { geo in
-                        HStack(spacing: 0) {
-                            Rectangle()
-                                .foregroundColor(battle.myTeamResult.key == .victory ?
-                                                    AppColor.spPink :
-                                                    AppColor.spLightGreen)
-                                .frame(width: CGFloat(barValue) * geo.size.width)
-                            
-                            Rectangle()
-                                .foregroundColor(battle.myTeamResult.key == .victory ?
-                                                    AppColor.spLightGreen :
-                                                    AppColor.spPink)
-                        }
+                    VStack(alignment: .trailing) {
+                        Text("\(record.rule)")
+                            .sp1Font(color: Color.secondary)
+                        
+                        Spacer()
+                        
+                        Text("\(record.stageName)")
+                            .sp2Font(
+                                color: isSelected ?
+                                    AppColor.recordRowTitleSelectedColor :
+                                    AppColor.recordRowTitleNormalColor
+                            )
+                            .padding(.bottom, 5)
                     }
-                    .frame(height: 5)
                 }
+                .padding([.leading, .trailing], 10)
                 
-                WebImage(url: battle.playerResult.player.weapon.imageURL)
-                    .resizable()
-                    .aspectRatio(1, contentMode: .fit)
-                    .frame(height: 60)
-                    .padding(.top, 20)
+                Spacer()
                 
-                if !record.isDetail {
-                    ProgressView()
+                GeometryReader { geo in
+                    HStack(spacing: 0) {
+                        Rectangle()
+                            .foregroundColor(record.victory ?
+                                                AppColor.spPink :
+                                                AppColor.spLightGreen)
+                            .frame(width: CGFloat(barValue) * geo.size.width)
+                        
+                        Rectangle()
+                            .foregroundColor(record.victory ?
+                                                AppColor.spLightGreen :
+                                                AppColor.spPink)
+                    }
                 }
+                .frame(height: 5)
             }
-            else {
-                EmptyView()
+            
+            WebImage(url: record.weaponImageURL)
+                .resizable()
+                .aspectRatio(1, contentMode: .fit)
+                .frame(height: 60)
+                .padding(.top, 20)
+            
+            if !record.isDetail {
+                ProgressView()
             }
         }
         .background(isSelected ? Color.accentColor : AppColor.battleListRowBackgroundColor)
@@ -114,15 +109,15 @@ struct RecordRow: View {
         
         #if os(macOS)
         contentView
+            .padding(.bottom)
         #else
         contentView
-            .padding([.leading, .trailing], 12)
         #endif
     }
 }
 
 struct BattleRow_Previews: PreviewProvider {
-
+    
     static var previews: some View {
         PreviewWrapper()
             .frame(width: 300, height: 230)
@@ -134,14 +129,14 @@ struct BattleRow_Previews: PreviewProvider {
     
     struct PreviewWrapper: View {
         @State var record: Record? = nil
-
+        
         var body: some View {
             VStack {
                 if let record = record {
-                RecordRow(record: record, isSelected: false, onSelected: { _ in })
-                    .frame(width: 300)
-                RecordRow(record: record, isSelected: true, onSelected: { _ in })
-                    .frame(width: 300)
+                    RecordRow(record: record, isSelected: false, onSelected: { _ in })
+                        .frame(width: 300)
+                    RecordRow(record: record, isSelected: true, onSelected: { _ in })
+                        .frame(width: 300)
                 }
             }
             .onAppear {
