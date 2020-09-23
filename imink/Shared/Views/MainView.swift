bb095f84b865dd50ed72e075019b01029ac163c5
Jone Wang <i@jone.wang>
2020-09-23 16:03:20 +0800

    Show navigation bar on iOS.

--- imink/Shared/Views/MainView.swift
+++ imink/Shared/Views/MainView.swift
@@ -28,7 +28,6 @@ struct MainView: View {
                             EmptyView()
                         }
                     }
-                    .navigationBarHidden(true)
                     
                     #else
                     
