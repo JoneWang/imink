5b64bce2065b7b5f14b0c0c4ea06ec38cedc4977
JoneWang <i@jone.wang>
2020-10-07 15:08:36 +0800

    Add SalmonRun schedule.

---
+++ imink/imink/Views/SwiftUI/Views/Views/SalmonRunScheduleView.swift
@@ -0,0 +1,103 @@
+//
+//  SalmonRunScheduleView.swift
+//  imink
+//
+//  Created by 王强 on 2020/10/6.
+//
+
+import SwiftUI
+import SDWebImageSwiftUI
+
+struct SalmonRunScheduleView: View {
+    let schedules: SP2SalmonRunSchedules
+    
+    private let scheduleTimeFormat: DateFormatter = {
+        let formatter = DateFormatter()
+        formatter.dateFormat = "MM/dd HH:mm"
+        return formatter
+    }()
+    
+    private var dataSource: [SP2SalmonRunSchedules.Schedule] {
+        schedules.schedules.map { schedule in
+            if let schedule = schedules.details.first(where: { $0.startTime == schedule.startTime }) {
+                return schedule
+            }
+            
+            return schedule
+        }
+    }
+    
+    var body: some View {
+        ForEach(dataSource, id: \.startTime) { schedule in
+            
+            VStack(spacing: 10) {
+                
+                VStack {
+                    Text("\(schedule.startDate, formatter: scheduleTimeFormat) - \(schedule.endDate, formatter: scheduleTimeFormat)")
+                        .sp2Font(size: 15, color: Color.primary)
+                        .colorInvert()
+                }
+                .padding(3)
+                .padding(.horizontal)
+                .background(Color.secondary)
+                .clipShape(Capsule())
+                
+                if let stage = schedule.stage,
+                   let weapons = schedule.weapons {
+                    HStack(alignment: .top) {
+                        
+                        VStack(spacing: 0) {
+                            
+                            Text("\(stage.name)")
+                                .sp2Font(color: Color.primary)
+                            
+                            WebImage(url: stage.imageURL)
+                                .resizable()
+                                .aspectRatio(640 / 360, contentMode: .fill)
+                                .frame(minWidth: 100)
+                                .cornerRadius(8)
+                                .padding(.top)
+                            
+                        }
+                        
+                        VStack(spacing: 0) {
+                            
+                            Text("Supplied Weapons")
+                                .sp2Font(color: Color.primary)
+                            
+                            VStack {
+                                
+                                Spacer()
+                                
+                                HStack(alignment: .center) {
+                                    
+                                    ForEach(weapons, id: \.id) { weapon in
+                                        WebImage(url: weapon.weapon.thumbnailURL)
+                                            .resizable()
+                                            .aspectRatio(1, contentMode: .fit)
+                                            .frame(minWidth: 40)
+                                            .cornerRadius(8)
+                                    }
+                                    
+                                }
+                                
+                                Spacer()
+                                
+                            }
+                            .padding(.top)
+                            
+                        }
+                        
+                    }
+                    
+                }
+                
+            }
+            .frame(maxWidth: .infinity)
+            .padding()
+            .background(Color.primary.opacity(0.1))
+            .cornerRadius(10)
+            
+        }
+    }
+}
