931afc08263ebfc93742e2aa2e10c22eda70eb97
Jone Wang <i@jone.wang>
2020-11-03 00:04:38 +0800

    Support Chinese Traditional. Update font.

--- imink/Shared/Theme/AppTheme.swift
+++ imink/Shared/Theme/AppTheme.swift
@@ -11,9 +11,9 @@ import SwiftUI
 struct AppTheme {
     static let defaultTextureSize = CGSize(width: 256, height: 128)
     
-    static let spFontName = "Splatfont 1 v1.001"
+    static let spFontName = "Splatfont 1 v1.002"
     
-    static let sp2FontName = "Splatfont 2 v1.003"
+    static let sp2FontName = "Splatfont 2 v1.004"
     
     static let spFont = UIFont(name: spFontName, size: 12)
     
