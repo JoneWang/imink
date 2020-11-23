f4583d2d149ef1d88abdeba3cd3a53aeb95d529a
Jone Wang <i@jone.wang>
2020-11-23 15:18:51 +0800

    Add InkCore.

---
+++ imink/InkCore/InkCore.h
@@ -0,0 +1,18 @@
+//
+//  InkCore.h
+//  InkCore
+//
+//  Created by Jone Wang on 2020/11/23.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for InkCore.
+FOUNDATION_EXPORT double InkCoreVersionNumber;
+
+//! Project version string for InkCore.
+FOUNDATION_EXPORT const unsigned char InkCoreVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <InkCore/PublicHeader.h>
+
+
