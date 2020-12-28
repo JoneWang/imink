2467e6f8013478042a66258502f544ee117842c8
Jone Wang <i@jone.wang>
2020-12-28 17:54:50 +0800

    Fix crash when getting InkCore bundle.

--- imink/InkCore/Bundle.swift
+++ imink/InkCore/Bundle.swift
@@ -10,8 +10,7 @@ import SwiftUI
 public extension Bundle {
     
     static var inkCore: Bundle? {
-        let path = Bundle.main.path(forResource: "InkCore", ofType: "framework", inDirectory: "Frameworks")!
-        return Bundle(path: path)
+        return Bundle(identifier: "jone.wang.InkCore")
     }
     
 }
