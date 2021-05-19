1fae0ef82ca9f183f8ec21d73e0e0c04a22eeec8
Jone Wang <i@jone.wang>
2021-05-19 20:59:45 +0800

    Fix the bug that SalmonRun stage are not displayed.

--- imink/Shared/Utils/StageImage.swift
+++ imink/Shared/Utils/StageImage.swift
@@ -8,16 +8,16 @@
 import SplatNet2
 
 private let stageImageNameMap = [
-    "Spawning Grounds_img": "Spawning Grounds",
-    "シェケナダム_img": "Spawning Grounds",
-    "Marooner's Bay_img": "Marooner's Bay",
-    "難破船ドン・ブラコ_img": "Marooner's Bay",
-    "Lost Outpost_img": "Lost Outpost",
-    "海上集落シャケト場_img": "Lost Outpost",
-    "Salmonid Smokeyard_img": "Salmonid Smokeyard",
-    "トキシラズいぶし工房_img": "Salmonid Smokeyard",
-    "Ruins of Ark Polaris_img": "Ruins of Ark Polaris",
-    "朽ちた箱舟 ポラリス_img": "Ruins of Ark Polaris",
+    "Spawning Grounds": "Spawning Grounds",
+    "シェケナダム": "Spawning Grounds",
+    "Marooner's Bay": "Marooner's Bay",
+    "難破船ドン・ブラコ": "Marooner's Bay",
+    "Lost Outpost": "Lost Outpost",
+    "海上集落シャケト場": "Lost Outpost",
+    "Salmonid Smokeyard": "Salmonid Smokeyard",
+    "トキシラズいぶし工房": "Salmonid Smokeyard",
+    "Ruins of Ark Polaris": "Ruins of Ark Polaris",
+    "朽ちた箱舟 ポラリス": "Ruins of Ark Polaris",
 ]
 
 extension SalmonRunSchedules.Schedule.Stage {
