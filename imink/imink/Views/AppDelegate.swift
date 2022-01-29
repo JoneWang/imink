e3f44f63fbb62aa7b881fe6f4e4df2650ab4fe0a
Xander Deng <dengxiang2010@gmail.com>
2022-01-29 18:13:45 +0800

    fix airplay mirror

--- imink/imink/Views/AppDelegate.swift
+++ imink/imink/Views/AppDelegate.swift
@@ -41,6 +41,9 @@ class SceneDelegate: UIResponder, UIWindowSceneDelegate {
     func scene(_ scene: UIScene, willConnectTo session: UISceneSession, options connectionOptions: UIScene.ConnectionOptions) {
         let contentView = InkApp()
 
+        // Do not config external display. So we can get default mirroring behavior.
+        guard session.role != .windowExternalDisplay else { return }
+
         if let windowScene = scene as? UIWindowScene {
             let window = UIWindow(windowScene: windowScene)
             window.rootViewController = UIHostingController(rootView: contentView)
